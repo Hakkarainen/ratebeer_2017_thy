d66362802a707eed9bdd79f77dc441926cea3c63
Hakkarainen <thyyppa@gmail.com>
2017-01-29 21:38:05 +0200

    Commit 2017_01_29_week2_Teht16_saakka_thy

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,12 @@
 class Beer < ActiveRecord::Base
+    include RatingAverage
+
 belongs_to :brewery
+has_many :ratings, dependent: :destroy
+
+def to_s
+    b = Brewery.find_by id: self.brewery_id
+    "#{b.to_s} / #{self.name}"
+end
+
 end
---
+++ app/models/rating.rb
@@ -0,0 +1,7 @@
+class Rating < ActiveRecord::Base
+  belongs_to :beer
+  def to_s
+    b = Beer.find_by id: self.beer_id
+    "#{b.name} / #{self.score}"
+  end
+end
